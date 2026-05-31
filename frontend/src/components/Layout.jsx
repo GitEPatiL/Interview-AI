d382a0eb2502fd1ed64d7b55de53649c8d818a75
Aviraj Gite <giteaviraj155@gmail.com>
2026-06-01 01:00:02 +0530

    Hide navbar on interview dashboard page

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import Navbar from './Navbar';
 
 const Layout = () => {
+  const location = useLocation();
+  const isInterviewPage = location.pathname.startsWith('/interview');
+
   return (
     <div className="min-h-screen flex flex-col relative w-full overflow-x-hidden">
-      <Navbar />
+      {!isInterviewPage && <Navbar />}
       <main className="flex-1 w-full h-full relative">
         <Outlet />
       </main>
